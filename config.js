28f45e8341e30d345a0d9e674fcb6ee288d2fae4
maiersk <poilyoungman@foxmail.com>
2019-12-29 21:19:03 +0800

    v1

---
+++ config.js
@@ -0,0 +1,19 @@
+const APPKEY = "985438D9C169CDF9C8303DB02B15C629";
+
+const rulearr = {
+    1 : {"warning" : "进服请耐心等待 勿离开~~. 就再等等嘛~"},
+    2 : {"warning" : "处于内测阶段. 看到重启通知请保存物品 "},
+    3 : {"danger" : "不能恶意刷物品,副本,炸弹. (～￣。￣)→别哦"},
+    4 : {"danger" : "不要擅自冒充管理员或工作人员. 拒绝无理事宜"},
+    5 : {"success" : "区域制的沙盒玩法，有pvp/pve/murder~"},
+    6 : {"success" : "QQ群297842208内有订阅地址. 进服慢就去吧"},
+    7 : {"primary" : "对我们有任何问题或者建议请联系 腐竹"},
+    8 : {"light" : "文明游戏"}
+}
+
+const imgarr = [
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577561396477&di=0cb05f78564ea10932b04fc21e5c6fa1&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1817430070%2C2947041924%26fm%3D214%26gp%3D0.jpg",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577561468105&di=fe1802d04963734f310937abb4832c5a&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F16f8e594d9471acb3579af837c2f416fa423aa78.jpg",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577561639557&di=013c2c3d71c5afabf14a1319d33800da&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2017-10-11%2F59ddbd58dc5c0.png",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577620197660&di=6c6993ccd27beae59801c9a315003607&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F599c8b3c94666a567a05750e582c433a4bf00604.png"
+]
---
+++ js/index.js
@@ -0,0 +1,71 @@
+function getParam() {
+    var url = location.search;
+    var json = {};
+    if (url.indexOf("?") != -1) {
+        var strparam = url.substr(1);
+        var params = strparam.split("&");
+        
+        for (var i = 0; i < params.length; i++) {
+            json[params[i].split("=")[0]] = params[i].split("=")[1];
+        }
+    }
+    return json;
+}
+
+$(function() {
+    
+    // 获取页面请求参数
+    let params = {};
+    params = getParam();
+    // console.log(params);
+
+
+
+    // 使用steamapi获取玩家信息
+    var tojsonp = "https://json2jsonp.com/?url=";
+    var steamapi = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" + APPKEY + "&steamids=" + params.steamids;
+    var url = tojsonp + encodeURIComponent(steamapi) //+ "&callback=CALL_BACK";
+    // console.log(url)
+
+    const name = $(".personal #steamName");
+    const avatar = $(".personal img");
+
+    $.ajax({
+        url : url,
+        type : "GET",
+        dataType:"jsonp",
+        success : (data) => {
+            steamobj = data.response.players[0]
+            // console.log(steamobj);
+
+            name.html(steamobj.personaname);
+            avatar.attr("src", steamobj.avatarfull);
+            // console.log(steamobj.personaname)
+        },
+        error: function() {
+            console.error("异常！");
+        }
+    })
+
+
+
+    // 刷新页面更换图片
+    const bgimg = $(".bg");
+    bgimg.css("background-image", "url(" + imgarr[Math.floor(Math.random() * imgarr.length)] + ")")
+    // console.log(imgarr[Math.round(Math.random() * imgarr.length)]);
+
+
+
+    // 遍历配置文件服务器规则数据
+    const ruleListGp = $(".list-group");
+
+    if (rulearr) {
+        for (var _ in rulearr) {
+            for (var k in rulearr[_]) {
+                ruleListGp.append("<li class='list-group-item list-group-item-" + k + "'>" + rulearr[_][k] +  "</li>")
+                // console.log(rulearr[_][k])
+            }
+
+        }
+    }
+})
